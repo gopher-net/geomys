dfd94f95fe0f0295cebd193749928e8dcee5b9f5
Matt Oswalt <matt@keepingitclassless.net>
2015-01-15 23:24:10 -0500

    Implementing Data Structures for RIPv2
    Wrote basic data structures for storing a RIP routing database.
    
    Here's a usage example, perhaps from georipd.go:
        https://gist.github.com/05e0309a08f4828449f6
    
    A struct for md5 authentication is included, but I haven't done much with it yet.
    
    Change-Id: Ieb1aeef1d2018eff31c2facc1b5d1082b7576491
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

---
+++ geomys/georipd/obj/ripobj.go
@@ -0,0 +1,64 @@
+/*
+    Object definitions for RIP.
+    Currently only supporting RIPv2
+
+    Author: Matt Oswalt
+*/
+
+package obj
+
+import (
+    "time"
+)
+
+// Hash Table for storing all Rip Messages, keyed off of sender
+var RipDB map[uint32]RipMessage
+
+// Housekeeping, you want fluff pillow?
+func init() {
+    RipDB = make(map[uint32]RipMessage)
+}
+
+// Struct for RIP Messages
+type RipMessage struct {
+    LastUpdate      time.Time
+    Command         uint8
+    Version         uint8
+    Routes          []RipRoute
+}
+
+// Struct for RIP Authentication Headers
+type RipAuth struct {
+    AuthType        uint16
+    DigestOffset    uint16
+    KeyID           uint8
+    AuthDataLen     uint8
+    SeqNum          uint32
+    AuthData        string // No uint128 type, so have to do string
+}
+
+// Struct for Individual Networks advertised by RIP
+type RipRoute struct {
+    AddrFamily      uint16
+    RouteTag        uint16
+    IpAddr          uint32
+    Netmask         uint32
+    NextHop         uint32
+    Metric          uint32
+}
+
+// Set RipMessage for received message per sender
+func UpdateNbrRoutes(cmd uint8, version uint8, sender uint32, routes []RipRoute) {
+    var thisMsg RipMessage
+    thisMsg.LastUpdate = time.Now().UTC()
+    thisMsg.Command = cmd
+    thisMsg.Version = version
+    thisMsg.Routes = routes
+
+    RipDB[sender] = thisMsg
+}
+
+// Retrieves current RIP Table
+func GetRoutes() map[uint32]RipMessage {
+    return RipDB
+}
